a1c1f38dea6d3fd25c1e3d4cc610c091c3ef4ea8
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-16 22:09:35 +0300

    Digits product (*)

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4  -> 24
+// 5  -> 120
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Please type a number");
+
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine($"Value:{Result(number)}");
+
+
+
+int Result(int N)
+{
+      int A =1;
+      for(int i=1; i<=N; i++)
+      {
+        A *=i;
+      }
+      return A;
+}
