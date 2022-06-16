459d3704de07aedcc1bb9da3095b65f9ae9c13de
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-16 22:07:08 +0300

    1 - number of digits

---
+++ Task01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Please type a number");
+
+
+int number = Convert.ToInt32(ReadLine());
+
+WriteLine($"Number of digits:{NumDigits(number)}");
+
+int NumDigits(int N)
+{
+      int i = 0;
+        while (N!=0)
+        {
+            N=N/10;
+            i++;
+        }
+     
+    return i;
+}
